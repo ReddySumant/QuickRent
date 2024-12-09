363526a24d5b1a54f0c15ab1208940e519ea1ff0
Shraddha <ssj.shraddha.jadhav@gmail.com>
2024-12-10 01:47:26 +0530

    Changes done

--- Quickrent-backend/src/main/java/com/quickrent/dao/orderDao.java
+++ Quickrent-backend/src/main/java/com/quickrent/dao/orderDao.java
@@ -1,9 +1,14 @@
 package com.quickrent.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.quickrent.pojo.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
-public interface orderDao extends JpaRepository<Order, Integer> { }
+import java.util.List;
+
+public interface OrderDao extends JpaRepository<Order, Integer> {
+
+    // Fetch orders by user ID
+    @Query("SELECT o FROM Order o WHERE o.user.id = :userId")
+    List<Order> findOrdersByUserId(@Param("userId") Integer userId);
+}
